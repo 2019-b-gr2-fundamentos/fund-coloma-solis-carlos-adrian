9d7370f19c2c580b1a35f47628b8a3ac009b6b0a
Adrian Coloma <carlos.coloma@epn.edu.ec>
2019-10-24 12:17:43 -0500

    Clases

---
+++ Clases/01-variables.js
@@ -0,0 +1,25 @@
+// Comentario
+/* <- abro  bloque de comentario
+Nombre: Adrian Coloma
+Fecha de Actualizacion: 17/10/2019
+Descripcion: Ejemplos de variables primitivas en JS
+-> cierro bloque comentario */
+// Primitivas (tipos de variables)
+/*
+int   -> Numero ENTERO Ej: 5
+char  -> Caracter      Ej: a o b o c
+float -> Numeros decimales Ej: 12.12 o -14.2
+double -> N. decimales ++ Ej: 1212.....
+string -> Cadena de caracteres Ej: "abc" o "cd"
+null   -> VACIO  Ej: null
+boolean -> Verdadero / Falso Ej: true o false 
+*/
+// var edad = 30; "Ya no se utiliza" // 
+//let edad = 30; // Mutable -> Cambiar 
+//const edad = 30; // Inmutable .> No Cambiar
+// edad = 30 -> python
+// var edad: integer = 30; -> kotlin Mutable
+// val edad: integer = 30; -> kotlin 
+//const IVA = 30;
+//let sueldo = 0;
+//IVA = 14; // no esta permitido 
---
+++ Clases/03-numero-strings.ts
@@ -0,0 +1,3 @@
+let resultado ;
+resultado = 2%3 ;
+console.log("pera"+2);
