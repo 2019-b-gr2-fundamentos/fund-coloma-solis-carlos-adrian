6304934c04fa60b6b96b1643548654ef6a397493
Adrian Coloma <carlos.coloma@epn.edu.ec>
2019-10-30 14:58:12 -0500

    30/10/2019

---
+++ Deberes/Pseudocódigo.ts
@@ -0,0 +1,5 @@
+// Producto Punto
+// Si se quiere realizar el producto punto entre 2 vectores
+// de dos componentes entonces insertar la componente 1 del primer vector ,
+// la componente 2 del primer vector, la componente 1 del segundo vector y la componente 2 del segundo vector.
+// Finalmente multiplicar componente a componente y luego sumar los resultados
