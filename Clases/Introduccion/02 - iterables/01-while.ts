d4b85f82f8661218d4fbda3fdbbdc8626e43fe0b
Adrian Coloma <carlos.coloma@epn.edu.ec>
2019-11-06 17:54:40 -0500

    Clase iterables

--- Clases/Introduccion/02 - iterables/01-while.ts
+++ Clases/Introduccion/02 - iterables/01-while.ts
@@ -1,6 +1,6 @@
 // DO WHILE
-let vecesQueHeComido = 0;
-console.log('Vamos a comer :D')
+//let vecesQueHeComido = 0;
+//console.log('Vamos a comer :D')
 /*do {
     vecesQueHeComido = vecesQueHeComido + 1
     switch (vecesQueHeComido){         
@@ -17,7 +17,7 @@ console.log('Vamos a comer :D')
             console.log('Gordito')
             break;         
     }
-} while (vecesQueHeComido < 4)*/
+} while (vecesQueHeComido < 4)
 while (vecesQueHeComido <4){
     vecesQueHeComido = vecesQueHeComido + 1;
     switch (vecesQueHeComido){         
@@ -34,7 +34,27 @@ while (vecesQueHeComido <4){
             console.log('Gordito')
             break;      
 }
+}*/
+let entrada = 0
+let Anali_socio = 0
+let Algebra = 0
+let Compiladores = 0
+do {
+    entrada = entrada + 1
+    if (entrada%3 ==0){
+        Compiladores = Compiladores + 1
+    } else{
+    if (entrada%2 == 0){
+        Algebra = Algebra + 1
+    } else{
+        Anali_socio = Anali_socio + 1
+    }
 }
+} while(entrada < 50);
+console.log(`Analisis: ${Anali_socio}`);
+console.log("Algebra: "+ Algebra);
+console.log("Compiladores: "+ Compiladores);
+console.log(Anali_socio + Algebra + Compiladores)
 /*
 1 Analisis socioeconomico
 2 Algebra
@@ -43,4 +63,7 @@ while (vecesQueHeComido <4){
 7 Calculo
 11 Fisica
 13 Ingles
-*/ 
+Seudocodigo: 
+Si es impar -> aumentamos a Analisis socioeconomico
+Si es par -> aumentamos a algebra 
+Si es multiplo de 3 -> aumentamos a compiladores */
