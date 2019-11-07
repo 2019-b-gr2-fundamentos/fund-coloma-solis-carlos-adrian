74db8bf04ca81a95eb9149165bb427c5b102c48b
Adrian Coloma <carlos.coloma@epn.edu.ec>
2019-11-07 12:36:16 -0500

    07/11/2019

--- Clases/hola.py
+++ Clases/hola.py
@@ -1,2 +1,11 @@
-x = "hola \n mundo"
-print(x)
+i = 0
+pares = 0
+impares = 0
+while i < 1000:
+    i = i + 1
+    if i%2 ==0:
+        pares = pares + 1
+    else:
+        impares = impares +1    
+print("Pares: " + str(pares))
+print("Impares: " + str(impares))            
