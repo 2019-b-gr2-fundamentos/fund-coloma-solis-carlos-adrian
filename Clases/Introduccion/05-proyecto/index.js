6b6e952b852761f22f5003b1d47c740b46af412b
Adrian Coloma <carlos.coloma@epn.edu.ec>
2020-01-06 16:04:46 -0500

    06/01/2019

---
+++ Clases/Introduccion/05-proyecto/index.js
@@ -0,0 +1,4 @@
+// NODE MODULES
+var sumaLibreria = require('libreria-adrian/lib/suma');
+var respuesta = sumaLibreria(1, 2);
+console.log('La respuesta es: ', respuesta);
---
+++ Clases/Introduccion/05-proyecto/index.ts
@@ -0,0 +1,4 @@
+// NODE MODULES
+const sumaLibreria = require('libreria-adrian/lib/suma')
+const respuesta = sumaLibreria(1,2);
+console.log('La respuesta es: ',respuesta)
